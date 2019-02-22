9e1a2270a645a3f72ba3211b2118a72e3e8f20f5
Truls Aagaard <truls.aa@gmail.com>
2019-02-22 09:42:42 +0100

    Updates

---
+++ precache-manifest.82d0eaa938859e5e4f33c7d860d848aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b62be57665586e418328",
+    "url": "/react-substitutions-ui5-webparts/static/css/main.311bfc70.chunk.css"
+  },
+  {
+    "revision": "b62be57665586e418328",
+    "url": "/react-substitutions-ui5-webparts/static/js/main.b62be576.chunk.js"
+  },
+  {
+    "revision": "971b58026f7f91d16335",
+    "url": "/react-substitutions-ui5-webparts/static/js/runtime~main.971b5802.js"
+  },
+  {
+    "revision": "f7740b050f4ed0467463",
+    "url": "/react-substitutions-ui5-webparts/static/js/2.f7740b05.chunk.js"
+  },
+  {
+    "revision": "619772be1649c50cbeb670f5379de91a",
+    "url": "/react-substitutions-ui5-webparts/static/media/SAP-icons.619772be.ttf"
+  },
+  {
+    "revision": "f5597eb2c59ef17574898f25262ab422",
+    "url": "/react-substitutions-ui5-webparts/static/media/SAP-icons.f5597eb2.woff"
+  },
+  {
+    "revision": "7d7f4b769ea6fe65509c808558234540",
+    "url": "/react-substitutions-ui5-webparts/static/media/SAP-icons.7d7f4b76.woff2"
+  },
+  {
+    "revision": "75db1e665be9ed5e8e509c3063d5e1e7",
+    "url": "/react-substitutions-ui5-webparts/index.html"
+  }
+];
