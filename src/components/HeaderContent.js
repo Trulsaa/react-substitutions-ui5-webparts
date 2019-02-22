4d7d23705439248aeb6094ee4575fd1e01c505b4
Truls Aagaard <truls.aa@gmail.com>
2019-02-22 10:17:21 +0100

    Made it more substituiony

--- src/components/HeaderContent.js
+++ src/components/HeaderContent.js
@@ -1,23 +1,19 @@
-import React, { Component } from "react";
-import styled from "styled-components";
-import "@ui5/webcomponents/dist/ShellBar";
 import "@ui5/webcomponents/dist/Icon";
 import "@ui5/webcomponents/dist/Input";
+import "@ui5/webcomponents/dist/ShellBar";
+
+import React from "react";
 
 export default () => (
   <ui5-shellbar
     id="shellbar"
-    primary-title="Corporate Portal"
-    secondary-title="secondary title"
-    profile="../../../../../../www/samples/images/woman_avatar_5.png"
+    primary-title="Substitutions"
+    logo="https://upload.wikimedia.org/wikipedia/commons/5/59/SAP_2011_logo.svg"
     notification-count="99+"
     show-notifications
     show-product-switch
     show-co-pilot
   >
-    <div>
-      <ui5-icon src="nav-back" data-ui5-slot="icon" />
-      <ui5-input data-ui5-slot="searchField" />
-    </div>
+    <div />
   </ui5-shellbar>
 );
--- src/components/ListContent.js
+++ src/components/ListContent.js
@@ -3,119 +3,32 @@ import styled from "styled-components";
 import "@ui5/webcomponents/dist/List";
 import "@ui5/webcomponents/dist/StandardListItem";
 
+const people = [
+  { name: "Lars" },
+  { name: "Ivar" },
+  { name: "Tone" },
+  { name: "Karl" },
+  { name: "Katrine" },
+  { name: "Oda" },
+  { name: "Vemund" },
+  { name: "Kristian" },
+  { name: "Kåre" },
+  { name: "Bjarne" },
+  { name: "Siri" },
+  { name: "Silje" },
+  { name: "Line" },
+  { name: "Tom" },
+  { name: "André" },
+  { name: "Jim" },
+  { name: "Øyvind" },
+  { name: "Ole" }
+];
 export default () => (
   <ui5-list id="myList" class="full-width">
-    <ui5-li
-      icon="sap-icon://nutrition-activity"
-      type="Active"
-      description="Tropical plant with an edible fruit"
-    >
-      Pineapple
-    </ui5-li>
-    <ui5-li
-      icon="sap-icon://nutrition-activity"
-      type="Active"
-      description="Occurs between red and yellow"
-    >
-      Orange
-    </ui5-li>
-    <ui5-li
-      icon="sap-icon://nutrition-activity"
-      type="Active"
-      description="The yellow lengthy fruit"
-    >
-      Banana
-    </ui5-li>
-    <ui5-li
-      icon="sap-icon://nutrition-activity"
-      type="Active"
-      description="The tropical stone fruit"
-    >
-      Mango
-    </ui5-li>
-    <ui5-li
-      icon="sap-icon://nutrition-activity"
-      type="Active"
-      description="Tropical plant with an edible fruit"
-    >
-      Pineapple
-    </ui5-li>
-    <ui5-li
-      icon="sap-icon://nutrition-activity"
-      type="Active"
-      description="Occurs between red and yellow"
-    >
-      Orange
-    </ui5-li>
-    <ui5-li
-      icon="sap-icon://nutrition-activity"
-      type="Active"
-      description="The yellow lengthy fruit"
-    >
-      Banana
-    </ui5-li>
-    <ui5-li
-      icon="sap-icon://nutrition-activity"
-      type="Active"
-      description="The tropical stone fruit"
-    >
-      Mango
-    </ui5-li>
-    <ui5-li
-      icon="sap-icon://nutrition-activity"
-      type="Active"
-      description="Tropical plant with an edible fruit"
-    >
-      Pineapple
-    </ui5-li>
-    <ui5-li
-      icon="sap-icon://nutrition-activity"
-      type="Active"
-      description="Occurs between red and yellow"
-    >
-      Orange
-    </ui5-li>
-    <ui5-li
-      icon="sap-icon://nutrition-activity"
-      type="Active"
-      description="The yellow lengthy fruit"
-    >
-      Banana
-    </ui5-li>
-    <ui5-li
-      icon="sap-icon://nutrition-activity"
-      type="Active"
-      description="The tropical stone fruit"
-    >
-      Mango
-    </ui5-li>
-    <ui5-li
-      icon="sap-icon://nutrition-activity"
-      type="Active"
-      description="Tropical plant with an edible fruit"
-    >
-      Pineapple
-    </ui5-li>
-    <ui5-li
-      icon="sap-icon://nutrition-activity"
-      type="Active"
-      description="Occurs between red and yellow"
-    >
-      Orange
-    </ui5-li>
-    <ui5-li
-      icon="sap-icon://nutrition-activity"
-      type="Active"
-      description="The yellow lengthy fruit"
-    >
-      Banana
-    </ui5-li>
-    <ui5-li
-      icon="sap-icon://nutrition-activity"
-      type="Active"
-      description="The tropical stone fruit"
-    >
-      Mango
-    </ui5-li>
+    {[...people, ...people, ...people].map(person => (
+      <ui5-li icon="sap-icon://employee" type="Active" description="Employee">
+        {person.name}
+      </ui5-li>
+    ))}
   </ui5-list>
 );
